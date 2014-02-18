2d1467d23f1526b101cb77d9e746e84cf746423b
Tyler Spivey <tspivey@pcdesk.net>
2014-02-18 15:10:27 -0800

    Move the stats list and update button onto a panel

--- profile_gui.py
+++ profile_gui.py
@@ -17,14 +17,6 @@ class ProfileActions(wx_forms.AutoSizedPanel):
   self.start.enable()
   self.stop.disable()
 
- def handle_update_stats(self):
-  func_stats = yappi.get_func_stats()
-  func_stats.sort(SORT_ORDERS[self.parent.sort_order.get_index()], 'desc')
-  if len(func_stats) > 50:
-   func_stats = func_stats[:50]
-  self.parent.stats.set_value(func_stats)
-  self.parent.stats.set_index(0)
-
  def handle_clear(self):
   yappi.clear_stats()
   self.parent.stats.clear()
@@ -32,7 +24,6 @@ class ProfileActions(wx_forms.AutoSizedPanel):
  start = fields.Button(label="&Start", callback=handle_start)
  stop = fields.Button(label="St&op", callback=handle_stop, enabled=False)
  clear = fields.Button(label="&Clear", callback=handle_clear)
- update_stats = fields.Button(label="&Update Statistics", callback=handle_update_stats)
 
  def render(self, *args, **kwargs):
   super(ProfileActions, self).render(*args, **kwargs)
@@ -48,12 +39,34 @@ class StatsList(wx_forms.SmartList):
  average_time = wx_fields.SmartColumn(title="Average Time", model_field='tavg')
 
 
+class StatsPanel(wx_forms.AutoSizedPanel):
+
+ def __init__(self, function=None, *args, **kwargs):
+  super(StatsPanel, self).__init__(*args, **kwargs)
+  self.function = function
+  if self.function is not None:
+   self.function = function
+  else:
+   self.function = yappi.get_func_stats
+
+ def handle_update_stats(self):
+  func_stats = self.function()
+  func_stats.sort(SORT_ORDERS[self.sort_order.get_index()], 'desc')
+  if len(func_stats) > 50:
+   func_stats = func_stats[:50]
+  self.stats.set_value(func_stats)
+  self.stats.set_index(0)
+
+ stats = StatsList(label="Stats")
+ sort_order = fields.ComboBox(label="Sort", choices=FIELDS, read_only=True)
+ update_stats = fields.Button(label="&Update Statistics", callback=handle_update_stats)
+
 class ProfileGui(wx_forms.AutoSizedFrame):
+
+ stats_panel = StatsPanel()
 
  def handle_close(self):
   self.destroy()
 
- stats = StatsList(label="Stats")
- sort_order = fields.ComboBox(label="Sort", choices=FIELDS, read_only=True)
  actions = ProfileActions(sizer_type='horizontal')
  close = fields.Button(close=True, callback=handle_close)
